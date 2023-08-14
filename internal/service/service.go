f92d44875b550de27f3ebf60836070a1301e345e
muratovdias <muratovdias1601@gmail.com>
2023-08-14 10:38:25 +0600

    структура Service и интерфейс ToDo

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,15 @@
 package service
+
+import "github.com/muratovdias/todo-list-tt/internal/models"
+
+type ToDo interface {
+	CreateTask(models.ToDo) (string, error)
+	UpdateTask(models.ToDo) (int64, error)
+	DeleteTask(string) (int64, error)
+	MakeTaskDone(string) (int64, error)
+	TaskList(string) ([]models.ToDo, error)
+}
+
+type Service struct {
+	ToDo
+}
