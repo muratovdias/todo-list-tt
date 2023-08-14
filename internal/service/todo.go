8466b8e66dfdf729f26cd01f29e9bfe1f0daebe0
muratovdias <muratovdias1601@gmail.com>
2023-08-14 11:49:29 +0600

    реализация сервиса ToDo

--- internal/service/todo.go
+++ internal/service/todo.go
@@ -1,6 +1,9 @@
 package service
 
-import "github.com/muratovdias/todo-list-tt/internal/storage"
+import (
+	"github.com/muratovdias/todo-list-tt/internal/models"
+	"github.com/muratovdias/todo-list-tt/internal/storage"
+)
 
 type ToDoService struct {
 	store storage.ToDo
@@ -10,4 +13,29 @@ func NewTODOService(store storage.ToDo) *ToDoService {
 	return &ToDoService{
 		store: store,
 	}
+}
+
+func (t ToDoService) CreateTask(do models.ToDo) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t ToDoService) UpdateTask(do models.ToDo) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t ToDoService) DeleteTask(s string) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t ToDoService) MakeTaskDone(s string) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t ToDoService) TaskList(s string) ([]models.ToDo, error) {
+	//TODO implement me
+	panic("implement me")
 }
