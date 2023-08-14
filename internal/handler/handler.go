b9a0b01c193502f6c685fd268f7923259e2d59c2
muratovdias <muratovdias1601@gmail.com>
2023-08-14 11:52:46 +0600

    инициализация fiber роутера

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,6 +1,10 @@
 package handler
 
-import "github.com/muratovdias/todo-list-tt/internal/service"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/muratovdias/todo-list-tt/internal/config"
+	"github.com/muratovdias/todo-list-tt/internal/service"
+)
 
 type Handler struct {
 	service service.Service
@@ -10,4 +14,14 @@ func NewHandler(service service.Service) *Handler {
 	return &Handler{
 		service: service,
 	}
+}
+
+func (h *Handler) Routes(cfg *config.Config) *fiber.App {
+	app := fiber.New(fiber.Config{
+		ReadTimeout:  cfg.Timeout,
+		WriteTimeout: cfg.Timeout,
+		IdleTimeout:  cfg.IdleTimeout,
+	})
+
+	return app
 }
