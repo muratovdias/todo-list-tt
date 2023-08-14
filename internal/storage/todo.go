8b27c9cb8eaca183308110c7db5216f6b88a5bb2
muratovdias <muratovdias1601@gmail.com>
2023-08-14 12:26:00 +0600

    реализация метода CreateTask

--- internal/storage/todo.go
+++ internal/storage/todo.go
@@ -1,9 +1,14 @@
 package storage
 
 import (
+	"context"
+	"fmt"
 	"github.com/muratovdias/todo-list-tt/internal/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
+
+const path = "storage.todo."
 
 type ToDoStore struct {
 	collection *mongo.Collection
@@ -15,9 +20,14 @@ func NewTODOStore(collection *mongo.Collection) *ToDoStore {
 	}
 }
 
-func (t ToDoStore) CreateTask(do models.ToDo) (string, error) {
-	//TODO implement me
-	panic("implement me")
+func (t ToDoStore) CreateTask(todo models.ToDo) (string, error) {
+	res, err := t.collection.InsertOne(context.Background(), todo)
+
+	if err != nil {
+		return "", fmt.Errorf("%sCreateTask: %w", path, err)
+	}
+
+	return res.InsertedID.(primitive.ObjectID).Hex(), nil
 }
 
 func (t ToDoStore) UpdateTask(do models.ToDo) (int64, error) {
