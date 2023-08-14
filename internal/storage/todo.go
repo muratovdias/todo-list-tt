a3fabc9f7a52d981b55bf6adc170afdabbeca62f
muratovdias <muratovdias1601@gmail.com>
2023-08-14 11:34:14 +0600

    реализация интерфейса ToDo

--- internal/storage/todo.go
+++ internal/storage/todo.go
@@ -1,6 +1,9 @@
 package storage
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"github.com/muratovdias/todo-list-tt/internal/models"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type ToDoStore struct {
 	collection *mongo.Collection
@@ -10,4 +13,29 @@ func NewTODOStore(collection *mongo.Collection) *ToDoStore {
 	return &ToDoStore{
 		collection: collection,
 	}
+}
+
+func (t ToDoStore) CreateTask(do models.ToDo) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t ToDoStore) UpdateTask(do models.ToDo) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t ToDoStore) DeleteTask(s string) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t ToDoStore) MakeTaskDone(s string) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t ToDoStore) TaskList(s string) ([]models.ToDo, error) {
+	//TODO implement me
+	panic("implement me")
 }
