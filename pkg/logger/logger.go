8db03f6b52e9eda6a3185c6e38cd87c0d31f64e6
muratovdias <muratovdias1601@gmail.com>
2023-08-14 11:21:09 +0600

    logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,15 @@
 package logger
+
+import (
+	"golang.org/x/exp/slog"
+	"os"
+)
+
+func SetupLogger() *slog.Logger {
+	var log *slog.Logger
+	log = slog.New(
+		slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+	)
+
+	return log
+}
