37f85ff3474dc167b98a9b74eacc9641bc3f3101
muratovdias <muratovdias1601@gmail.com>
2023-08-14 10:41:11 +0600

    структура и конструктор Handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,13 @@
 package handler
+
+import "github.com/muratovdias/todo-list-tt/internal/service"
+
+type Handler struct {
+	service service.Service
+}
+
+func NewHandler(service service.Service) *Handler {
+	return &Handler{
+		service: service,
+	}
+}
